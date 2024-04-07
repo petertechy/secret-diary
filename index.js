207bcc61c56433658a385bdf1b867b3255f5d726
Olanrewaju <ikolabaolanrewaju@gmail.com>
2024-04-07 19:45:15 +0100

    fourth addiction

--- index.js
+++ index.js
@@ -19,34 +19,43 @@ function addNote() {
 
 function displayNote() {
   let d = "";
-  let cat;
   for (let i = 0; i < noteArray.length; i++) {
-    if(cat == "Sport"){
-      document.getElementById("exampleModal").style.borderLeft ="red"
+    let cat = noteArray[i].category; // Assuming you have a 'category' property in your note objects
+
+    // Set border color based on category
+    let borderColor = "";
+    if (cat === "Sport") {
+      borderColor = "red";
+    } else if (cat === "Church") {
+      borderColor = "blue";
+    } else if (cat === "Event") {
+      borderColor = "green";
+    } else if (cat === "Party") {
+      borderColor = "orange";
+    } else {
+      borderColor = "black"; // Default border color
     }
-    d += `<tr onclick='popModal(${i})' data-bs-toggle="modal" data-bs-target="#exampleModal" class="mine">
+
+    d += `<tr onclick='popModal(${i})' data-bs-toggle="modal" data-bs-target="#exampleModal" class="mine" style="border-left: 5px solid ${borderColor};">
               <td>${i + 1}</td>
               <td>
                   <p>${noteArray[i].title}</p>
               </td>
-              <td>${new Date(
-                noteArray[i].createdDate
-              ).toLocaleDateString()}
+              <td>${new Date(noteArray[i].createdDate).toLocaleDateString()}
               </td>
               <td>
-                  <button class = "btn btn-danger" onclick="deleteNote(${i})">Delete</button>
+                  <button class="btn btn-danger" onclick="deleteNote(${i})">Delete</button>
               </td>
           </tr>`;
   }
   document.getElementById("display").innerHTML = d;
- 
 }
 
 function deleteNote(i) {
   if (confirm(`Are you sure you want to delete ${noteArray[i].title}`)) {
-      noteArray.splice(i, 1);
-      console.log(noteArray);
-      displayNote();
+    noteArray.splice(i, 1);
+    console.log(noteArray);
+    displayNote();
   }
 }
 
@@ -71,7 +80,7 @@ function popModal(i) {
   document.getElementById("exampleModal").innerHTML = modalContent;
 }
 
-function editModal(i){
+function editModal(i) {
   let modalContent = `
     <div class="modal-dialog">
       <div class="modal-content">
@@ -89,14 +98,37 @@ function editModal(i){
     </div>
   `;
   document.getElementById("exampleModal").innerHTML = modalContent;
-
 }
-function updateModal(i){
+function updateModal(i) {
   let newTitle = document.getElementById("editTitle").value;
   let newNote = document.getElementById("editNote").value;
   noteArray[i].title = newTitle;
   noteArray[i].note = newNote;
-  console.log(noteArray)
+  console.log(noteArray);
   displayNote();
-
+  $("#exampleModal").modal("hide"); // Hide the modal after updating the note
 }
+
+// When the "Add Category" button is clicked
+document.getElementById("openModalBtn").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "block";
+});
+
+// When the "Close" button in the modal is clicked
+document.getElementById("closeModalBtn").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "none";
+});
+
+  // When the "Add Category" button in the modal is clicked
+  document.getElementById("addCategoryBtn").addEventListener("click", function() {
+    var categoryName = document.getElementById("categoryName").value;
+    var categoryColor = document.getElementById("categoryColor").value;
+    var selectElement = document.getElementById("category");
+    var option = document.createElement("option");
+    option.text = categoryName;
+    option.style.backgroundColor = categoryColor;
+    selectElement.add(option);
+    document.getElementById("myModal").style.display = "none";
+    document.getElementById("categoryName").value = "";
+    document.getElementById("categoryColor").value = "#000000"; // Reset color to black
+  });
